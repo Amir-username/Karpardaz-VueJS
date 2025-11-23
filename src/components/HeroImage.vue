67226149853d3787229d735c45dae77713844ba6
amir <najiamirdesktop@gmail.com>
2025-11-23 12:40:46 +0330

    add base fonts

--- src/components/HeroImage.vue
+++ src/components/HeroImage.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts"></script>
 <template>
-  <div class="hero-image"></div>
+  <div class="hero-image">
+    <h1 class="lalezar">تیتر اصلی</h1>
+  </div>
 </template>
 <style scoped>
 .hero-image {
   height: 100vmax;
   width: 100%;
-  background-image: url(../assets/HeroImage.jpg);
+  /* background-image: url(../assets/HeroImage.jpg); */
 }
 </style>
